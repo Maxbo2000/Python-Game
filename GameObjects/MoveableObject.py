0417409af41b3717bea4ade6a594c55763109f54
Maxbo <maxbo2000@web.de>
2023-02-22 18:00:18 +0100

    MasterClass(Monster, Player)
    Made the MasterClass for Monsters, the Player and maybe other objects

--- GameObjects/MoveableObject.py
+++ GameObjects/MoveableObject.py
@@ -1,2 +1,25 @@
-class MoveableObject:
-    __Lives : int = 0
+from abc import ABCMeta, abstractmethod
+
+
+class MoveableObject(metaclass=ABCMeta):
+    def __init__(self): pass
+
+    @property
+    @abstractmethod
+    def _Health(self): pass
+
+    @property
+    @abstractmethod
+    def _Name(self): pass
+
+    @property
+    @abstractmethod
+    def _WalkSpeed(self): pass
+
+    @property
+    @abstractmethod
+    def _HitBox_X(self): pass
+
+    @property
+    @abstractmethod
+    def _HitBox_Y(self): pass
