4586fae9d5d3221094dc18691c5711854af7db54
Maxbo <maxbo2000@web.de>
2023-02-24 09:18:07 +0100

    Getter and Setter for MasterClass
    Also updated some other crap and made the Player init call compatible again

--- GameObjects/MoveableObject.py
+++ GameObjects/MoveableObject.py
@@ -2,7 +2,7 @@ from pygame import Rect
 
 
 class MoveableObject:
-    def __init__(self, health, name, walkspeed, hitbox_x, hitbox_y, height, width):
+    def __init__(self, health, name, walkspeed, hitbox_x, hitbox_y, height, width, x, y):
         self.__Health = health
         self.__Name = name
         self.__WalkSpeed = walkspeed
@@ -10,16 +10,32 @@ class MoveableObject:
         self.__HitBox_Y = hitbox_y
         self.__HitBox_H = height
         self.__HitBox_W = width
+        self.__Pos_X = x
+        self.__Pos_Y = y
 
     __Health: int
     __Name: str
     __WalkSpeed: float
+    __Pos_X: int
+    __Pos_Y: int
     __HitBox_X: int
     __HitBox_Y: int
     __HitBox_H: int
     __HitBox_W: int
 
+    def getHealth(self): return self.__Health
+    def getName(self): return self.__Name
+    def getWalkSpeed(self): return self.__WalkSpeed
+    def getPosX(self): return self.__Pos_X
+    def getPosY(self): return self.__Pos_Y
+    def setHealth(self, value): self.__Health = value
+    def setName(self, value): self.__Name = value
+    def setWalkSpeed(self, value): self.__WalkSpeed = value
+    def setPosX(self, value): self.__Pos_X = value
+    def setPosY(self, value): self.__Pos_Y = value
+    def setHitboxX(self, value): self.__HitBox_X = value
+    def setHitBoxY(self, value): self.__HitBox_Y = value
+
     def makeHitBox(self):
         hitbox = Rect(self.__HitBox_X, self.__HitBox_Y, self.__HitBox_H, self.__HitBox_W)
         return hitbox
-
--- GameObjects/Player.py
+++ GameObjects/Player.py
@@ -1,7 +1,7 @@
 from GameObjects.MoveableObject import MoveableObject
 
 class Player(MoveableObject):
-    super().__init__(100, "test", 1.00, 10, 10)
+    super().__init__(100, "test", 1.00, 10, 10, 10, 10, 0, 0)
     sprite: None
     inv: None
 
