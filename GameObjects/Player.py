6be1066ff034f4121610bb586118cb60b4acabf8
IzayaIzaya <ceciwacker@gmail.com>
2023-03-02 19:41:03 +0100

    I created the handel-key , move_left,move_right,move_up ,move_dowmethod

--- GameObjects/Player.py
+++ GameObjects/Player.py
@@ -1,3 +1,5 @@
+import pygame
+
 from GameObjects.MoveableObject import MoveableObject
 
 class Player(MoveableObject):
@@ -5,4 +7,27 @@ class Player(MoveableObject):
         super().__init__(health, name, walkspeed, hitbox_x, hitbox_y, height, width, x, y)
         sprite: None
         inv: None
+
+        def handle_keys(self):
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                self.move_left()
+            if keys[pygame.K_RIGHT]:
+                self.move_right()
+            if keys[pygame.K_UP]:
+                self.move_up()
+            if keys[pygame.K_DOWN]:
+                self.move_down()
+
+        def move_left(self):
+            self.x -= self.walkspeed
+
+        def move_right(self):
+            self.x += self.walkspeed
+
+        def move_up(self):
+            self.y -= self.walkspeed
+
+        def move_down(self):
+            self.y += self.walkspeed
 
--- Main.py
+++ Main.py
@@ -5,26 +5,36 @@ from GameObjects.Player import Player
 if __name__ == '__main__':
     print('PyCharm')
 
-
 pygame.init()
 player = Player(100, "test", 1.00, 10, 10, 40, 80, 0, 0)
 pygame.display.set_caption("Humans vs Monster")
 screen = pygame.display.set_mode([600, 600])
 
-
-screen.fill((0, 0, 0))
 clock = pygame.time.Clock()
-pygame.draw.rect(screen,(255,255,0), (player.getPosY(),player.getPosX(),player.getHitBox_H(),player.getHitBox_W()))
-#pygame.draw.rect(screen, (255, 255, 0), (player.getPosX(),player.getPosY(),player.getHitboxW(),player.getHitboxH()))
 
-#Um das Fenster offen zulassen-------------
-pygame.display.update()
 while True:
+    # Bildschirm schließen
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            quit()
-#--------------------------
-clock.tick(60)
+            pygame.quit()
+            sys.exit()
 
+    # Bewegungssteuerung
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        player.move_left()
+    if keys[pygame.K_RIGHT]:
+        player.move_right()
+    if keys[pygame.K_UP]:
+        player.move_up()
+    if keys[pygame.K_DOWN]:
+        player.move_down()
 
-#print(player.getHealth())
+    # Bildschirm-Update
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, (255, 255, 0), (player.getPosY(), player.getPosX(), player.getHitBox_H(), player.getHitBox_W()))
+    pygame.display.update()
+
+    # Framerate-Limit
+    clock.tick(60)
+
