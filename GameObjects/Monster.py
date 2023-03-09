4c7a142541e1fde3913c559d1c7b9bb46c4d1eb3
Maxbo <maxbo2000@web.de>
2023-03-09 03:24:11 +0100

    Created Collision
    Changed a lot of the master Class as well as Player and Monster
    Gave Healthbar it's own class so code stays relatively clean and not bloated

--- GameObjects/Monster.py
+++ GameObjects/Monster.py
@@ -1,11 +1,28 @@
 import math
 
+import pygame
+
 from GameObjects.MoveableObject import MoveableObject
 
+
 class Monster(MoveableObject):
-    def __init__(self, health=int, name=str, walkspeed=int, height=int, width=int, x=int, y=int):
-        super().__init__(health, name, walkspeed, height, width, x, y)
-        sprite: None
+    __SpriteHeight = 60
+    __SpriteWidth = 60
+
+    def __init__(self, health=int, name=str, walkspeed=int, x=int, y=int):
+        super().__init__(health, name, walkspeed, x, y)
+        self.sprite = pygame.transform.scale(pygame.image.load("Graphic/monster.png"), (self.__SpriteHeight, self.__SpriteWidth))
+        self.rect = self.sprite.get_rect()
+
+    def get_height(self):
+        return self.rect.height
+
+    def get_width(self):
+        return self.rect.width
+
+    def draw(self, screen):
+        self.setHitbox(pygame.Rect(self.getPosX(), self.getPosY(), self.__SpriteHeight, self.__SpriteWidth))
+        screen.blit(self.sprite, (self.getPosX(), self.getPosY()))
 
     def move(self, player_x: int, player_y: int):
         dx = player_x - self.getPosX()
--- GameObjects/MoveableObject.py
+++ GameObjects/MoveableObject.py
@@ -2,12 +2,10 @@ from pygame import Rect
 
 
 class MoveableObject:
-    def __init__(self, health, name, walkspeed, height, width, x, y):
+    def __init__(self, health, name, walkspeed, x, y):
         self.__Health = health
         self.__Name = name
         self.__WalkSpeed = walkspeed
-        self.__HitBox_H = height
-        self.__HitBox_W = width
         self.__Pos_X = x
         self.__Pos_Y = y
 
@@ -16,24 +14,45 @@ class MoveableObject:
     __WalkSpeed: int
     __Pos_X: int
     __Pos_Y: int
-    __HitBox_X: int
-    __HitBox_Y: int
-    __HitBox_H: int
-    __HitBox_W: int
+    __HitBox: Rect
 
-    def getHealth(self): return self.__Health
-    def getName(self): return self.__Name
-    def getWalkSpeed(self): return self.__WalkSpeed
-    def getPosX(self): return self.__Pos_X
-    def getPosY(self): return self.__Pos_Y
-    def getHitBox_H(self): return self.__HitBox_H
-    def getHitBox_W(self): return self.__HitBox_W
-    def setHealth(self, value): self.__Health = value
-    def setName(self, value): self.__Name = value
-    def setWalkSpeed(self, value): self.__WalkSpeed = value
-    def setPosX(self, value): self.__Pos_X = value
-    def setPosY(self, value): self.__Pos_Y = value
+    # Getter
+    def getHealth(self):
+        return self.__Health
 
-    def makeHitBox(self):
-        hitbox = Rect(self.__HitBox_X, self.__HitBox_Y, self.__HitBox_H, self.__HitBox_W)
-        return hitbox
+    def getName(self):
+        return self.__Name
+
+    def getWalkSpeed(self):
+        return self.__WalkSpeed
+
+    def getPosX(self):
+        return self.__Pos_X
+
+    def getPosY(self):
+        return self.__Pos_Y
+
+    def getHitbox(self):
+        return self.__HitBox
+
+    # Setter
+    def setHealth(self, value: int):
+        if value > 0:
+            self.__Health = value
+        else:
+            self.__Health = 0
+
+    def setName(self, value: str):
+        self.__Name = value
+
+    def setWalkSpeed(self, value: int):
+        self.__WalkSpeed = value
+
+    def setPosX(self, value: int):
+        self.__Pos_X = value
+
+    def setPosY(self, value: int):
+        self.__Pos_Y = value
+
+    def setHitbox(self, value: Rect):
+        self.__HitBox = value
